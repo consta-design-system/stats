6a23e5dbffebff669fa8542c7aaa673c7fa9d768
gizeasy <gizeasy@gmail.com>
2022-08-25 17:12:15 +0300

    chore(stand): new stand and new builder

---
+++ src/components/Stats/__stand__/Stats.stand.tsx
@@ -0,0 +1,11 @@
+import { createStand } from '##/stand/standConfig';
+
+export default createStand({
+  title: 'Stats',
+  id: 'Stats',
+  group: 'components',
+  description:
+    'Этот компонент нужен, чтобы показывать важные числа — на графиках, схемах, картах или сами по себе',
+  version: '1.0.3',
+  status: 'stable',
+});
