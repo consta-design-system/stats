ae3989c4c0f6fa57817757c66759575790be498f
N1MBER <socialfox33@gmail.com>
2022-09-13 17:33:50 +0300

    docs(sandbbox): add sandboxes links
    consta-design-system/uikit#2615

--- src/components/Stats/__stand__/Stats.stand.tsx
+++ src/components/Stats/__stand__/Stats.stand.tsx
@@ -7,5 +7,7 @@ export default createStand({
   description:
     'Этот компонент нужен, чтобы показывать важные числа — на графиках, схемах, картах или сами по себе',
   version: '1.0.3',
+  sandbox:
+    'https://codesandbox.io/embed/stats-gkbtlc?fontsize=14&hidenavigation=1&theme=dark',
   status: 'stable',
 });
