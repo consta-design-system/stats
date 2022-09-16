bec9a038e3f8a19781682ea20e0c24a8ad175bcf
gizeasy <gizeasy@gmail.com>
2022-09-16 10:25:53 +0300

    Update Stats.stand.tsx

--- src/components/Stats/__stand__/Stats.stand.tsx
+++ src/components/Stats/__stand__/Stats.stand.tsx
@@ -1,11 +1,8 @@
 import { createStand } from '##/stand/standConfig';
 
-import image from './Stats.image.svg';
-
 export default createStand({
   title: 'Stats',
   id: 'Stats',
-  image,
   group: 'components',
   description:
     'Компонент для отображения чисел, с заголовком, единицами измерения, иконками и другими настройками.',
