1a94021a56bfddd25e954271aef7cfc473eba2f1
gizeasy <gizeasy@gmail.com>
2022-09-16 10:32:38 +0300

    Docs/sandbox (#17)
    * docs(documentation): add doc for stats stand
    
    consta-design-system/uikit#2568
    
    * docs(sandbbox): add sandboxes links
    
    consta-design-system/uikit#2615
    
    * fix(sandbox): rewrite links to ids
    
    * Update yarn.lock
    
    * Update Stats.stand.tsx
    
    Co-authored-by: N1MBER <socialfox33@gmail.com>

--- src/components/Stats/__stand__/Stats.stand.tsx
+++ src/components/Stats/__stand__/Stats.stand.tsx
@@ -1,14 +1,12 @@
 import { createStand } from '##/stand/standConfig';
 
-import image from './Stats.image.svg';
-
 export default createStand({
   title: 'Stats',
   id: 'Stats',
-  image,
   group: 'components',
   description:
     'Компонент для отображения чисел, с заголовком, единицами измерения, иконками и другими настройками.',
   version: '1.0.3',
+  sandbox: 'stats-gkbtlc',
   status: 'stable',
 });
